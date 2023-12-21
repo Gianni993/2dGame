5541811dbaaf65467de5543afa4827a26898aea7
Gianni993 <cocciolo.giovanni93@gmail.com>
2023-12-21 11:37:07 +0100

    first commit

---
+++ src/entity/Player.java
@@ -0,0 +1,137 @@
+package entity;
+
+//import java.awt.Color;
+import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+
+import main.GamePanel;
+import main.KeyHandler;
+
+public class Player extends Entity{
+
+    File f1 = new File("./res/player/napoUp1.png");
+    File f2 = new File("./res/player/napoUp2.png");
+    File f3 = new File("./res/player/napoBack1.png");
+    File f4 = new File("./res/player/napoBack2.png");
+    File f5 = new File("./res/player/napoSxDown.png");
+    File f6 = new File("./res/player/napoSxUp.png");
+    File f7 = new File("./res/player/napoDxDown.png");
+    File f8 = new File("./res/player/napoDxUp.png");
+
+    GamePanel gp;
+    KeyHandler keyH;
+    
+    public Player (GamePanel gp, KeyHandler keyH){
+
+        this.gp = gp;
+        this.keyH = keyH;
+
+        setDefaultValues();
+        getPlayerimage();
+
+    }
+    public void setDefaultValues(){
+        //posizione iniziale di player 
+        x = 384;
+        y = 288;
+        speed = 4 ; //velocita 4px/frame
+        direction = "down"; //direzione iniziale
+    }
+
+
+    public void getPlayerimage(){
+
+        try {
+            
+            up1 = ImageIO.read(f1);
+            up2 = ImageIO.read(f2);
+            down1 = ImageIO.read(f3);
+            down2 = ImageIO.read(f4);
+            left1 = ImageIO.read(f5);
+            left2 = ImageIO.read(f6);
+            right1 = ImageIO.read(f7);
+            right2 = ImageIO.read(f8);
+            
+        }catch(IOException e){
+            e.printStackTrace();
+
+        }
+
+    }
+
+    public void update() {
+
+        //richiamo funzioni all handler e si collegano tramite le variabili player per far muovere il player
+        if(keyH.upPressed == true){     
+            direction = "up";
+            y -= speed; //java parte da x:0 y:0 in alto a sx quindi gli assi sono invertiti
+                    
+            }
+            else if (keyH.downPressed == true) {
+                direction = "down";
+                y += speed;
+            }
+            else if (keyH.leftPressed == true) {
+                direction = "left";
+                x -= speed;
+            }
+            else if (keyH.rightPressed == true) {
+                direction = "right";
+                x += speed;     
+            }
+
+           
+            spriteCounter++;
+            //cambia l immagine ogni 25 fotogrammi, per non far muovere sempre il personaggio
+            if(spriteCounter >25) {  //inserire una if ad inizio metodo update
+                if(spriteNum == 1){  // con condizione tutte e 4 le key == true con peratore or ||
+                    spriteNum = 2;
+                }
+                else if (spriteNum == 2) {
+                    spriteNum = 1;
+                }
+                spriteCounter = 0;
+            }
+            
+
+    }
+    public void draw(Graphics2D g2){
+
+      //  g2.setColor(Color.green);
+      //  g2.fillOval(x, y, gp.tileSize,gp.tileSize);  //disegna un rettangolo sullo schermo(coordinate, grandezza)
+
+      BufferedImage image = null;
+
+        switch (direction) {
+            case "up":
+                if (spriteNum == 1){image = up1;}
+                if (spriteNum == 2){image = up2;}       
+                break;
+        
+            case "down":
+                if (spriteNum == 1){image = down1;}
+                if (spriteNum == 2){image = down2;}
+                break;
+            
+            case "left":
+                if (spriteNum == 1){image = left1;}
+                if (spriteNum == 2){image = left2;}                
+                break;
+
+            case "right":
+                if (spriteNum == 1){image = right1;}
+                if (spriteNum == 2){image = right2;}                
+                break;
+
+            default:
+                break;
+        }
+        //disegna immagine con posizione x,y grandezza gp
+        g2.drawImage(image, x, y, gp.tileSize,gp.tileSize, null); //https://stackoverflow.com/questions/19212990/what-is-an-imageobserver
+    }
+
+}
---
+++ src/main/GamePanel.java
@@ -0,0 +1,106 @@
+package main;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+
+import javax.swing.JPanel;
+
+import entity.Player;
+import tile.TileManager;
+
+public class GamePanel extends JPanel implements Runnable{
+
+    // SCREEN SETTINGS
+    final int originalTileSize = 16;                    //dimensione originale personaggi npc e tessere
+    final int scale = 3;                                //fattore per resize
+
+    public final int tileSize = originalTileSize * scale;      //scala finale personaggi/tessera 48x48px
+    public final int maxScreenCol = 16;                        // numero di colonne
+    public final int maxScreenRow = 12;                        // numero di righe (rapporto 4:3)
+    public final int screenWidth = tileSize * maxScreenCol;    // larghezza schermo 48px tessera x 16 tessere 768px
+    public final int screenHeight = tileSize * maxScreenRow;   // altezza schermo 48px tessera x 12 tessere 576px
+
+    //FPS
+    int FPS = 60;
+
+    TileManager tileM = new TileManager(this);
+    KeyHandler keyH = new KeyHandler();
+    Thread gameThread;                                  //clock del gioco per creazione game loop
+    Player player = new Player(this, keyH);
+
+    public GamePanel() {                                //funzione costruttrice
+
+        this.setPreferredSize(new Dimension(screenWidth,screenHeight));
+        this.setBackground(Color.BLACK);                //colore background
+        this.setDoubleBuffered(true);             //www.tutorialspoint.com/what-is-double-buffering-in-java
+        this.addKeyListener(keyH);                      //serve per riconoscere l input da tastiera
+        this.setFocusable(true);              //https://docs.oracle.com/javase%2Ftutorial%2Fuiswing%2F%2F/misc/focus.html
+    }
+
+    public void startGameThread(){                      //metodo start
+
+        gameThread = new Thread(this);
+        gameThread.start();
+    }
+
+
+    @Override
+    public void run() {
+
+        double drawInterval = 1000000000/FPS; // 0,01666sec
+        double delta = 0;
+        long lastTime = System.nanoTime();   //prende il primo orario 
+        long currentTime;
+        //Fps display
+        long timer = 0;
+        int drawCount = 0;
+
+
+                //LOOP GAME (delta)
+            while (gameThread != null){ 
+
+                currentTime = System.nanoTime();        //prende l ora corrente all inizio del ciclo
+                delta += (currentTime - lastTime) / drawInterval; //si calcola la differenza tra l ultimo orario e quello appena preso e la sottrae al tempo degli fps targhet incrementando il valore di delta stesso
+                timer += (currentTime - lastTime);
+                lastTime = currentTime;// scambio dell ora corrente a ultima ora
+                
+                
+                if(delta >= 1){ //stampa qaundo delta è maggiore di 1
+                    //UPDATE AND DRAW
+                    //1)aggiornare le coordinate del personaggio 
+                    update();
+                     //2)stamapre a video 
+                    repaint();//chiamata strana al metodo paintComponent
+                    delta--; //toglie 1 a delta
+                    drawCount++;//fps
+
+            }   //Fps display
+                if(timer >=1000000000 ){
+                    System.out.println("Fps:" + drawCount);
+                    drawCount = 0;
+                    timer = 0;
+                }
+
+            }
+        }
+    public void update(){
+        
+        player.update();
+
+    }
+
+    public void paintComponent(Graphics g){
+        super.paintComponent(g);
+        
+        //sfera bianca
+        Graphics2D g2 = (Graphics2D)g;
+        
+        tileM.draw(g2);//prima lo sfondo
+        player.draw(g2);// e dopo i player
+
+        g2.dispose();           //utilizzato per rilasciare le risorse mantenute da una finestra o da un componente GUI.
+    }
+
+
+}
